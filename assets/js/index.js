6ae926254effcc55dafad62e599af0d0361c2533
Ansar Smagulov <ansar@anfragment.net>
2025-12-11 18:43:48 +0500

    Refactor mobile menu functionality into index.js; add resource inlining

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,9 @@
-(() => {
+document.addEventListener('DOMContentLoaded', function() {
+  setupOSButtons();
+  setupMobileMenu();
+});
+
+function setupOSButtons() {
   const os = getOS();
   if (os === null) {
     console.error('OS not recognized');
@@ -12,19 +17,39 @@
   elementsToHide.forEach((element) => {
     element.classList.add('hidden');
   });
+}
 
-  function getOS() {
-    const platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
-      macosPlatforms = ['macOS', 'Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
-  
-    if (macosPlatforms.indexOf(platform) !== -1) {
-      return 'macos';
-    } else if (windowsPlatforms.indexOf(platform) !== -1) {
-      return 'windows';
-    } else if (/Linux/.test(platform)) {
-      return 'linux';
-    }
-    return null;
+function setupMobileMenu() {
+  const hamburger = document.querySelector('.heading__hamburger');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  if (hamburger && mobileMenu) {
+    hamburger.addEventListener('click', function() {
+      const isExpanded = hamburger.getAttribute('aria-expanded') === 'true';
+
+      if (isExpanded) {
+        hamburger.setAttribute('aria-expanded', false);
+        mobileMenu.classList.toggle('mobile-menu--open');
+        document.body.style.overflow = '';
+      } else {
+        hamburger.setAttribute('aria-expanded', true);
+        mobileMenu.classList.toggle('mobile-menu--open');
+        document.body.style.overflow = 'hidden';
+      }
+    });
   }
-})();
+}
+
+function getOS() {
+  const platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+    macosPlatforms = ['macOS', 'Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    return 'macos';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    return 'windows';
+  } else if (/Linux/.test(platform)) {
+    return 'linux';
+  }
+  return null;
+}
