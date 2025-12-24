d6136e816d97186d934b9b9aed2eed633f5dd58a
Ansar Smagulov <ansar@anfragment.net>
2025-12-25 04:23:41 +0500

    Reorg resources

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  setupOSButtons();
   setupMobileMenu();
 });
-
-function setupOSButtons() {
-  const os = getOS();
-  if (os === null) {
-    console.error('OS not recognized');
-    return;
-  }
-  const elementsToShow = [...document.querySelectorAll(`[data-os="${os}"]`), ...document.querySelectorAll(`[data-os="identified"]`)];
-  elementsToShow.forEach((element) => {
-    element.classList.remove('hidden');
-  });
-  const elementsToHide = document.querySelectorAll(`[data-os="other"]`);
-  elementsToHide.forEach((element) => {
-    element.classList.add('hidden');
-  });
-}
 
 function setupMobileMenu() {
   const hamburger = document.querySelector('.heading__hamburger');
@@ -37,19 +20,4 @@ function setupMobileMenu() {
       }
     });
   }
-}
-
-function getOS() {
-  const platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
-    macosPlatforms = ['macOS', 'Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
-
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    return 'macos';
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    return 'windows';
-  } else if (/Linux/.test(platform)) {
-    return 'linux';
-  }
-  return null;
 }
